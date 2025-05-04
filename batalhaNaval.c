bc2731d43f2edaea45e9111102f0571bbd9ccfc9
gabriel-duarte01 <vitorgabriel.contato2@gmail.com>
2025-05-04 17:13:40 +0000

    tentando desenvolver o código

--- batalhaNaval.c
+++ batalhaNaval.c
@@ -4,18 +4,15 @@ int main(){
 
     char *tabuleiro[10][10] = {{"A", "B", "C", "D", "E", "F", "G", "H", "I", "J"}, {"1","2","3","4","5","6","7","8","9","10"} };
 
-    int linha = 0;
-    int coluna = 0;
+    //tem que fixar uma linha e fazer ela interagir com os elementos do outro vetor
+    //depois fazer avançar de linha e ela fazer o mesmo
 
-        while(linha <= 9){
 
-            printf("%s \n", tabuleiro[1]);
-
-            for(coluna; coluna <= 9; coluna++){
-                printf("%s ", tabuleiro[0][0]);
-            }
+        for(tabuleiro[0][0]; tabuleiro[0][0]; tabuleiro[0][0]++){
+            printf("%s \n %s", tabuleiro[0][0], tabuleiro[0][0]);
             
-            linha++;
+
+           
         }
 
 
