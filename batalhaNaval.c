84bb1d31a2d580492a4259f00e6f4e23ca144f3a
gabriel-duarte01 <vitorgabriel.contato2@gmail.com>
2025-05-06 17:26:11 +0000

    trabalhando no código

--- batalhaNaval.c
+++ batalhaNaval.c
@@ -1,21 +1,47 @@
 #include <stdio.h>
 
-int main(){
+#define TAMANHO  10;
 
-    char *tabuleiro[10][10] = {{"A", "B", "C", "D", "E", "F", "G", "H", "I", "J"}, {"1","2","3","4","5","6","7","8","9","10"} };
+int tabuleiro[i][j];
 
-    //tem que fixar uma linha e fazer ela interagir com os elementos do outro vetor
-    //depois fazer avançar de linha e ela fazer o mesmo
+i = TAMANHO;
+j = TAMANHO;    
 
+void iniciandoTabuleiro (int tabuleiro[i][j]){
 
-        for(tabuleiro[0][0]; tabuleiro[0][0]; tabuleiro[0][0]++){
-            printf("%s \n %s", tabuleiro[0][0], tabuleiro[0][0]);
-            
+    for (int i = 0; i < 10; i++){
+        for (int j = 0; j < 10; j++){
 
-           
+            tabuleiro[i][j] = 0;
         }
+    }
 
+}
 
+//navio 1
+tabuleiro [1][0];
+tabuleiro [2][0];
+tabuleiro [3][0];
 
-    return 0;
+//navio 2
+tabuleiro [5][6];
+tabieiro[6][6];
+tabuleiro[7][6];
+
+void exibirTabuleiro(int tabuleiro[i][j]){
+
+    for (int i = 0; i < 10; i++){
+        printf("%2d", tabuleiro[i]);
+
+        for (int j = 0; j < 10; j++){
+            printf("%d", tabuleiro[j]);
+        }
+    }
+
+}
+
+int main (){
+
+    iniciandoTabuleiro(tabuleiro[i][j]);
+    exibirTabuleiro(tabuleiro[i][j]);
 }
