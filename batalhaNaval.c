d703788e00ac654a19b8f689e127e863b5f652fe
gabriel-duarte01 <vitorgabriel.contato2@gmail.com>
2025-05-07 17:47:24 +0000

    adicionei alguns comentários

--- batalhaNaval.c
+++ batalhaNaval.c
@@ -8,7 +8,7 @@ void iniciandoTabuleiro (int tabuleiro[tamanho][tamanho]){
     for (int i = 0; i < tamanho; i++){
         for (int j = 0; j < tamanho; j++){
 
-            tabuleiro[i][j] = 0;
+            tabuleiro[i][j] = 0; //inicia o tabuleiro com zero
         }
     
     }
@@ -18,9 +18,9 @@ tabuleiro [2][0] = 3;
 tabuleiro [3][0] = 3;
 
 //navio 2
-tabuleiro [5][6] = 3;
-tabuleiro[6][6] = 3;
-tabuleiro[7][6] = 3;
+tabuleiro [4][6] = 3;
+tabuleiro[4][7] = 3;
+tabuleiro[4][8] = 3;
 
 }
 
@@ -28,7 +28,9 @@ tabuleiro[7][6] = 3;
 void exibirTabuleiro(int tabuleiro[tamanho][tamanho]){
     printf("    A B C D E F G H I J \n");
     for (int i = 0; i < tamanho; i++){
-        printf("%2d  ", i + 1);
+        printf("%2d  ", i + 1); //%2d faz os números ficarem alinhados 
+                                //os nº devem ser expressos com pelo menos 2 espaços de largura
+                                //se não tiver, quando chegasse na linha '10', eles desalinhariam
 
         for (int j = 0; j < tamanho; j++){
             printf("%d ", tabuleiro[i][j] );
