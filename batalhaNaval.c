03d68fe48144676e4ec381c99e59d3c3ed68a14a
gabriel-duarte01 <vitorgabriel.contato2@gmail.com>
2025-05-12 17:52:38 +0000

    tentando criar as diagonais prin. e secundária

--- batalhaNaval.c
+++ batalhaNaval.c
@@ -1,6 +1,8 @@
 #include <stdio.h>
 
 #define tamanho  10
+//tentar criar variáveis diagonais prin. e sec.
+//em vez de igualar os i e j, iguala elas
    
 
 void iniciandoTabuleiro (int tabuleiro[tamanho][tamanho]){
@@ -9,18 +11,10 @@ void iniciandoTabuleiro (int tabuleiro[tamanho][tamanho]){
         for (int j = 0; j < tamanho; j++){
 
             tabuleiro[i][j] = 0; //inicia o tabuleiro com zero
+            
         }
     
     }
-//navio 1
-tabuleiro [1][0] = 3;
-tabuleiro [2][0] = 3;
-tabuleiro [3][0] = 3;
-
-//navio 2
-tabuleiro [4][6] = 3;
-tabuleiro[4][7] = 3;
-tabuleiro[4][8] = 3;
 
 }
 
@@ -28,12 +22,19 @@ tabuleiro[4][8] = 3;
 void exibirTabuleiro(int tabuleiro[tamanho][tamanho]){
     printf("    A B C D E F G H I J \n");
     for (int i = 0; i < tamanho; i++){
+
+        
         printf("%2d  ", i + 1); //%2d faz os números ficarem alinhados 
                                 //os nº devem ser expressos com pelo menos 2 espaços de largura
-                                //se não tiver, quando chegasse na linha '10', eles desalinhariam
+                                //se não tiver o %2d, quando chegasse na linha '10', eles desalinhariam
 
         for (int j = 0; j < tamanho; j++){
             printf("%d ", tabuleiro[i][j] );
+        if(i == j){
+            printf("3");
+        } else if(i + j == 9){
+            printf("3");
+        }
         }
         printf("\n");
     }
