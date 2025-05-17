0304cc3c8c158c77d0619623af6d40176719eb42
gabriel-duarte01 <vitorgabriel.contato2@gmail.com>
2025-05-17 22:13:57 +0000

    criei a diagonal principal e a secundária

--- batalhaNaval.c
+++ batalhaNaval.c
@@ -1,50 +1,42 @@
 #include <stdio.h>
 
-#define tamanho  10
-//tentar criar variáveis diagonais prin. e sec.
-//em vez de igualar os i e j, iguala elas
-   
+#define tamanho 10
 
-void iniciandoTabuleiro (int tabuleiro[tamanho][tamanho]){
-
-    for (int i = 0; i < tamanho; i++){
-        for (int j = 0; j < tamanho; j++){
-
-            tabuleiro[i][j] = 0; //inicia o tabuleiro com zero
-            
+void iniciandoTabuleiro(int tabuleiro[tamanho][tamanho]) {
+    for (int linha = 0; linha < tamanho; linha++) {
+        for (int coluna = 0; coluna < tamanho; coluna++) {
+            // Preenchendo diagonal principal
+            if (linha == coluna) {
+                tabuleiro[linha][coluna] = 3;
+            }
+            // Preenchendo diagonal secundária
+            else if (linha + coluna == 9) {
+                tabuleiro[linha][coluna] = 3;
+            }
+            // Preenchendo o restante com água
+            else {
+                tabuleiro[linha][coluna] = 0;
+            }
         }
-    
     }
-
 }
 
-
-void exibirTabuleiro(int tabuleiro[tamanho][tamanho]){
-    printf("    A B C D E F G H I J \n");
-    for (int i = 0; i < tamanho; i++){
-
-        
-        printf("%2d  ", i + 1); //%2d faz os números ficarem alinhados 
-                                //os nº devem ser expressos com pelo menos 2 espaços de largura
-                                //se não tiver o %2d, quando chegasse na linha '10', eles desalinhariam
-
-        for (int j = 0; j < tamanho; j++){
-            printf("%d ", tabuleiro[i][j] );
-        if(i == j){
-            printf("3");
-        } else if(i + j == 9){
-            printf("3");
-        }
+void exibirTabuleiro(int tabuleiro[tamanho][tamanho]) {
+    printf("   A B C D E F G H I J\n");
+    for (int linha = 0; linha < tamanho; linha++) {
+        printf("%2d ", linha + 1);
+        for (int coluna = 0; coluna < tamanho; coluna++) {
+            printf("%d ", tabuleiro[linha][coluna]);
         }
         printf("\n");
     }
-
 }
 
-int main (){
-
+int main() {
     int tabuleiro[tamanho][tamanho];
-    
+
     iniciandoTabuleiro(tabuleiro);
     exibirTabuleiro(tabuleiro);
+
+    return 0;
 }
